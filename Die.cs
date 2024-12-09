a429507fa8f4f7ee74aad765ad73b580a98a9aa7
Max Butler <158061940+maxbutler407@users.noreply.github.com>
2024-12-09 16:58:45 +0000

    Add files via upload

---
+++ Die.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOPAssessment2V2
+{
+    internal class Die
+    {
+        public int number { get; private set; }
+        private static Random rnd;
+
+        public Die()
+        {
+            rnd = new Random();
+            number = rnd.Next(1, 7);
+        }
+
+        public int Roll()
+        {
+            number = rnd.Next(1, 7);
+            return number;
+        }
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,70 @@
+﻿using OOPAssessment2V2;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool Menu = true;
+        while (Menu == true)
+        {
+            Menu = MainMenu();
+        }
+
+        static bool MainMenu()
+        {
+            Console.WriteLine("Please select your option:");
+            Console.WriteLine("1) Sevens Out");
+            Console.WriteLine("2) Three or More");
+            Console.WriteLine("3) View statstics data");
+            Console.WriteLine("4) Perform tests");
+
+            int option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    var SevensOut = new SevensOut();
+                    SevensOut.start();
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+            return MainMenu();
+        }
+    }
+        public class SevensOut
+
+        {
+        public void start()
+        {
+
+            Console.WriteLine("Sevens Out will now begin...");
+
+            Die roll1 = new Die();
+            Die roll2 = new Die();
+
+            Console.WriteLine($"\nOne dice rolled a {roll1}");
+            Console.WriteLine($"\nOne dice rolled a {roll2}");
+
+
+
+        }
+
+    }
+
+}
+    
+
+
